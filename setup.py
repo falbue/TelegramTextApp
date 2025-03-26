fe3aca1c046fe7caf029e341b819261a6c12ac5b
Falbue <cyansair05@gmail.com>
2025-03-26 17:09:27 +0500

    Та же ошибка исправлена до конца

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='TelegramTextApp',
-    version='0.4.9.2.2',
+    version='0.4.9.2.3',
     packages=find_packages(),
     install_requires=[
         'telebot',
