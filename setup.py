2026d06f4849e83ee73d267e7298240622a51a33
falbue GameSense <cyansair05@gmail.com>
2025-03-28 16:32:48 +0500

    ЕБАНЫЙ init

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='TelegramTextApp',
-    version='0.5.1.1',
+    version='0.5.1.2',
     packages=find_packages(),
     install_requires=[
         'telebot',
