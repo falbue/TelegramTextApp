b4daab841fc4ef3c3d7fe1426630f4b3e5eb9c03
Falbue <cyansair05@gmail.com>
2025-03-28 12:34:50 +0500

    Добавлен просмотр функций, котрые указаны в меню

--- TelegramTextApp/developer_application/static/js/settings-editors.js
+++ TelegramTextApp/developer_application/static/js/settings-editors.js
@@ -1,4 +1,3 @@
-// Функция для создания редактора CodeMirror
 function createCodeMirrorEditor(textareaId, mode = 'javascript', theme = 'monokai') {
     const textarea = document.getElementById(textareaId);
     if (!textarea) {
@@ -9,15 +8,14 @@ function createCodeMirrorEditor(textareaId, mode = 'javascript', theme = 'monoka
     const editor = CodeMirror.fromTextArea(textarea, {
         lineNumbers: true,
         mode: mode,
-                theme: theme, // Используем указанную тему
-                readOnly: false,
-                lineWrapping: true,
-                indentUnit: 4,
-                tabSize: 4,
-                smartIndent: true
-            });
+        theme: theme, // Используем указанную тему
+        readOnly: false,
+        lineWrapping: true,
+        indentUnit: 4,
+        tabSize: 4,
+        smartIndent: true
+    });
 
-        // Функция для автоматической подстройки высоты редактора
     function adjustHeight(editor) {
         const scrollInfo = editor.getScrollInfo();
         const newHeight = scrollInfo.height + scrollInfo.top;
@@ -33,3 +31,91 @@ function createCodeMirrorEditor(textareaId, mode = 'javascript', theme = 'monoka
 const editor1 = createCodeMirrorEditor('codeEditor1', 'javascript', 'dracula');
 const editor2 = createCodeMirrorEditor('codeEditor2', 'python', 'monokai');
 const editor3 = createCodeMirrorEditor('codeEditor3', 'python', 'monokai');
+
+function insertTextIntoEditor(editor, text) {
+    if (editor) {
+        editor.setValue(text);
+    } else {
+        console.error("Editor is not initialized.");
+    }
+}
+
+function GetCode() {
+    const currentUrl = window.location.href;
+    const urlParts = currentUrl.split('/'); 
+    const menuName = urlParts[urlParts.length - 1];
+    const url = `/menu/${menuName}`;
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then(jsonString => {
+        const codeEditor = document.getElementById('codeEditor1');
+        const data = JSON.parse(jsonString);
+
+        if (data.menu.function) {
+            const SectionFunction = document.getElementById("function");
+            SectionFunction.classList.remove("none");
+            fetch(`/function/${data.menu.function}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(CodeFunction => {
+                console.log(CodeFunction)
+                insertTextIntoEditor(editor2, CodeFunction);
+            })
+
+        }
+
+        if (data.menu.handler) {
+            const SectionFunction = document.getElementById("handler");
+            SectionFunction.classList.remove("none");
+            fetch(`/function/${data.menu.handler.function}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(CodeFunction => {
+                console.log(CodeFunction)
+                insertTextIntoEditor(editor3, CodeFunction);
+            })
+        }
+
+        const code = JSON.stringify(data.menu, null, 4);
+        insertTextIntoEditor(editor1, code);
+
+        const Name = document.getElementById("menu-name");
+        Name.textContent = menuName;
+
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('Failed to load menu data.');
+    });
+}
+
+GetCode()
