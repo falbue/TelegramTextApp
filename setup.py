ad4506603739fe76a98123ccd798398f548f2e87
Falbue <cyansair05@gmail.com>
2025-03-29 14:48:38 +0500

    Ошибка

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='TelegramTextApp',
-    version='0.5.3.1',
+    version='0.5.3.2',
     packages=find_packages(where="."),
     include_package_data=True,
     package_data={
